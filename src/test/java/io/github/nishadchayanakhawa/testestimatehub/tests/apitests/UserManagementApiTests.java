eb26a2e9cbef452df1a2f03e1e602582432e4d39
nisha <nisha@pune-desktop>
2023-10-30 19:50:00 +0530

    Test Estimate Hub initialized

---
+++ src/test/java/io/github/nishadchayanakhawa/testestimatehub/tests/apitests/UserManagementApiTests.java
@@ -0,0 +1,11 @@
+package io.github.nishadchayanakhawa.testestimatehub.tests.apitests;
+
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+public class UserManagementApiTests extends AbstractTestNGSpringContextTests {
+	@Test
+	public void userManagementTest() {
+		
+	}
+}
