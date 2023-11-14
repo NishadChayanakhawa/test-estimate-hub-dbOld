5335668f83cf012b87299518f5934a5e8e004b47
nisha <nisha@pune-desktop>
2023-11-14 16:39:24 +0530

    application configuration api completed

---
+++ src/main/java/io/github/nishadchayanakhawa/testestimatehub/model/ApplicationConfiguration.java
@@ -0,0 +1,82 @@
+package io.github.nishadchayanakhawa.testestimatehub.model;
+
+//import section
+//jpa libraries
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+//validation libraries
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+//lombok libraries
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * <b>Class Name</b>: ApplicationConfiguration<br>
+ * <b>Description</b>: Application configuration entity.<br>
+ * 
+ * @author nishad.chayanakhawa
+ */
+@Entity
+@Table(name = "TEH_APPLICATION_CONFIGURATION", uniqueConstraints = {
+		@UniqueConstraint(name = "TEH_APPLICATION_CONFIGURATION_UNQ", columnNames = "configurationKey") })
+@Getter
+@Setter
+@NoArgsConstructor
+public class ApplicationConfiguration {
+
+	// id
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	// dummy configuration key to validate case-insensitive uniqueness of
+	// application, module and sub-module
+	@Getter(AccessLevel.NONE)
+	@Setter(AccessLevel.NONE)
+	private String configurationKey;
+
+	// application name)
+	@NotBlank(message = "application {required.message}")
+	private String application;
+
+	// module name
+	@NotBlank(message = "module {required.message}")
+	private String module;
+
+	// sub-module name
+	@NotBlank(message = "subModule {required.message}")
+	private String subModule;
+
+	// base test script count
+	@Min(value = 1, message = "baseTestScriptCount {minimum-value.message} 1")
+	private int baseTestScriptCount;
+
+	/**
+	 * <b>Method Name</b>: prepare<br>
+	 * <b>Description</b>: before inserting or updating record, populate
+	 * configurationKey. configurationKey is dummy key to enforce case-insensitive
+	 * unique constraing on application + module + subModule combination. Spaces in
+	 * individual properties are also removed.<br>
+	 */
+	@PrePersist
+	@PreUpdate
+	private void prepare() {
+		// remove spaces from application, module and sub-module and concatenate them,
+		// separated by ':'
+		// assign concatenated value to configurationKey
+		try {
+			this.configurationKey = String.format("%s:%s:%s", this.application.toLowerCase().replace(" ", ""),
+					this.module.toLowerCase().replace(" ", ""), this.subModule.toLowerCase().replace(" ", ""));
+		} catch(NullPointerException e) {
+			this.configurationKey =null;
+		}
+	}
+}
---
+++ src/main/java/io/github/nishadchayanakhawa/testestimatehub/services/ApplicationConfigurationService.java
@@ -0,0 +1,98 @@
+package io.github.nishadchayanakhawa.testestimatehub.services;
+
+//import section
+//logger
+import org.slf4j.LoggerFactory;
+//java utils
+import java.util.List;
+//model mapper
+import org.modelmapper.ModelMapper;
+//spring libraries
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionSystemException;
+//spring data validation
+import org.springframework.dao.DataIntegrityViolationException;
+//models and repositories
+import io.github.nishadchayanakhawa.testestimatehub.model.ApplicationConfiguration;
+import io.github.nishadchayanakhawa.testestimatehub.model.dto.ApplicationConfigurationDTO;
+import io.github.nishadchayanakhawa.testestimatehub.repositories.ApplicationConfigurationRepository;
+//exceptions
+import io.github.nishadchayanakhawa.testestimatehub.services.exceptions.DuplicateEntityException;
+import io.github.nishadchayanakhawa.testestimatehub.services.exceptions.EntityNotFoundException;
+import io.github.nishadchayanakhawa.testestimatehub.services.exceptions.TransactionException;
+
+/**
+ * <b>Class Name</b>: ApplicationConfigurationService<br>
+ * <b>Description</b>: Service for processing Application Configuration.<br>
+ * 
+ * @author nishad.chayanakhawa
+ */
+@Service
+public class ApplicationConfigurationService {
+	// logger
+	private static ch.qos.logback.classic.Logger logger = (ch.qos.logback.classic.Logger)LoggerFactory.getLogger(ApplicationConfigurationService.class);
+
+	// autowired application configuration repository
+	@Autowired
+	private ApplicationConfigurationRepository applicationConfigurationRepository;
+
+	// autowired model mapper
+	@Autowired
+	private ModelMapper modelMapper;
+
+	/**
+	 * <b>Method Name</b>: save<br>
+	 * <b>Description</b>: Save application configuration.<br>
+	 * 
+	 * @param applicationConfigurationToSaveDTO
+	 * @return
+	 */
+	public ApplicationConfigurationDTO save(ApplicationConfigurationDTO applicationConfigurationToSaveDTO) {
+		logger.debug("Saving: {}", applicationConfigurationToSaveDTO);
+		try {
+			// persist application configuration
+			ApplicationConfigurationDTO applicationConfigurationSavedDTO = modelMapper.map(
+					this.applicationConfigurationRepository
+							.save(modelMapper.map(applicationConfigurationToSaveDTO, ApplicationConfiguration.class)),
+					ApplicationConfigurationDTO.class);
+			logger.debug("Saved: {}", applicationConfigurationToSaveDTO);
+			// return persisted application configuration
+			return applicationConfigurationSavedDTO;
+		} catch (DataIntegrityViolationException e) {
+			// throw exception when app-module-subModule combination is not unique
+			throw (DuplicateEntityException) new DuplicateEntityException("Application Configuration",
+					"application-module-subModule",
+					String.format("%s-%s-%s", applicationConfigurationToSaveDTO.getApplication(),
+							applicationConfigurationToSaveDTO.getModule(),
+							applicationConfigurationToSaveDTO.getSubModule()))
+					.initCause(e);
+		} catch (TransactionSystemException e) {
+			throw (TransactionException) new TransactionException(e).initCause(e);
+		}
+	}
+
+	public List<ApplicationConfigurationDTO> getAll() {
+		List<ApplicationConfigurationDTO> applicationConfigurations = this.applicationConfigurationRepository.findAll()
+				.stream().map(applicationConfiguration -> modelMapper.map(applicationConfiguration,
+						ApplicationConfigurationDTO.class))
+				.toList();
+		logger.debug("Application configurations found: {}", applicationConfigurations);
+		return applicationConfigurations;
+	}
+
+	public ApplicationConfigurationDTO get(Long id) {
+		logger.debug("Looking up application configuration for id: {}", id);
+		ApplicationConfigurationDTO applicationConfiguration = modelMapper.map(
+				this.applicationConfigurationRepository.findById(id)
+						.orElseThrow(() -> new EntityNotFoundException("Application Configuration", id)),
+				ApplicationConfigurationDTO.class);
+		logger.debug("Application configuration found: {}", applicationConfiguration);
+		return applicationConfiguration;
+	}
+
+	public void delete(Long id) {
+		logger.debug("Deleting application configuration id: {}", id);
+		this.applicationConfigurationRepository.deleteById(id);
+	}
+}
